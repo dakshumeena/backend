dde54d26bc34466ae5a5e33f4786a584acd5e5ee
dakshumeena <dakshumeena698@gmail.com>
2026-08-28 13:02:09 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    app.listen(process.env.PORT , () => {
+        console.log(` Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
